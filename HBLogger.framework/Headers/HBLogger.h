0528270eb74663181bc879d8fa4714706458195a
mehul.modi <mehul.modi@hiddenbrains.in>
2019-10-24 11:04:00 +0530

    Initial commit

---
+++ HBLogger.framework/Headers/HBLogger.h
@@ -0,0 +1,19 @@
+//
+//  HBLogger.h
+//  HBLogger
+//
+//  Created by hb on 24/09/19.
+//  Copyright © 2019 hb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HBLogger.
+FOUNDATION_EXPORT double HBLoggerVersionNumber;
+
+//! Project version string for HBLogger.
+FOUNDATION_EXPORT const unsigned char HBLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HBLogger/PublicHeader.h>
+
+#import "SSZipArchive.h"
